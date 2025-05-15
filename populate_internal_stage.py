4a85501eb28e600f26bbcc7bd0bf25e5914842ea
Nikhil Vatsa <vatsanikhil@gmail.com>
2025-05-15 21:34:51 +0530

    dag can be used now

--- populate_internal_stage.py
+++ populate_internal_stage.py
@@ -23,14 +23,6 @@ def get_snowpark_session() -> Session:
         "database": os.getenv("SNOWFLAKE_DATABASE"),
         "schema": os.getenv("SNOWFLAKE_SCHEMA")
     }
-    # connection_parameters = {
-    #    "ACCOUNT":os.getenv("SNOWFLAKE_ACCOUNT"),
-    #     "USER":"snowpark_user",
-    #     "PASSWORD":"Test@12$4",
-    #     "ROLE":"SYSADMIN",
-    #     "DATABASE":"sales_dwh",
-    #     "SCHEMA":"source"
-    # }
 
     return Session.builder.configs(connection_parameters).create()
 
@@ -54,48 +46,48 @@ def traverse_directory(directory,file_extension) -> list:
 def main():
     # Specify the directory path to traverse
     session=get_snowpark_session()
-    # directory_path = '3-region-sales-data'
-    # csv_file_name, csv_partition_dir , csv_local_file_path= traverse_directory(directory_path,'.csv')
-    # parquet_file_name, parquet_partition_dir , parquet_local_file_path= traverse_directory(directory_path,'.parquet')
-    # json_file_name, json_partition_dir , json_local_file_path= traverse_directory(directory_path,'.json')
-    # stage_location = '@sales_dwh.source.my_internal_stg'
+    directory_path = '3-region-sales-data'
+    csv_file_name, csv_partition_dir , csv_local_file_path= traverse_directory(directory_path,'.csv')
+    parquet_file_name, parquet_partition_dir , parquet_local_file_path= traverse_directory(directory_path,'.parquet')
+    json_file_name, json_partition_dir , json_local_file_path= traverse_directory(directory_path,'.json')
+    stage_location = '@sales_dwh.source.my_internal_stg'
 
-    # # print(csv_file_name)
+    # print(csv_file_name)
     
-    # csv_index = 0
-    # for file_element in csv_file_name:
-    #     put_result = ( 
-    #                     session.file.put( 
-    #                     csv_local_file_path[csv_index], 
-    #                     stage_location+"/"+csv_partition_dir[csv_index], 
-    #                     auto_compress=False, overwrite=True, parallel=10)
-    #                 )
-    #     #put_result(file_element," => ",put_result[0].status)
-    #     csv_index+=1
+    csv_index = 0
+    for file_element in csv_file_name:
+        put_result = ( 
+                        session.file.put( 
+                        csv_local_file_path[csv_index], 
+                        stage_location+"/"+csv_partition_dir[csv_index], 
+                        auto_compress=False, overwrite=True, parallel=10)
+                    )
+        #put_result(file_element," => ",put_result[0].status)
+        csv_index+=1
 
-    # parquet_index = 0
-    # for file_element in parquet_file_name:
+    parquet_index = 0
+    for file_element in parquet_file_name:
 
-    #     put_result = ( 
-    #                     session.file.put( 
-    #                     parquet_local_file_path[parquet_index], 
-    #                     stage_location+"/"+parquet_partition_dir[parquet_index], 
-    #                     auto_compress=False, overwrite=True, parallel=10)
-    #                 )
-    #     #put_result(file_element," => ",put_result[0].status)
-    #     parquet_index+=1
+        put_result = ( 
+                        session.file.put( 
+                        parquet_local_file_path[parquet_index], 
+                        stage_location+"/"+parquet_partition_dir[parquet_index], 
+                        auto_compress=False, overwrite=True, parallel=10)
+                    )
+        #put_result(file_element," => ",put_result[0].status)
+        parquet_index+=1
     
-    # json_index = 0
-    # for file_element in parquet_file_name:
+    json_index = 0
+    for file_element in parquet_file_name:
 
-    #     put_result = ( 
-    #                     session.file.put( 
-    #                     json_local_file_path[json_index], 
-    #                     stage_location+"/"+json_partition_dir[json_index], 
-    #                     auto_compress=False, overwrite=True, parallel=10)
-    #                 )
-    #     #put_result(file_element," => ",put_result[0].status)
-    #     json_index+=1  
+        put_result = ( 
+                        session.file.put( 
+                        json_local_file_path[json_index], 
+                        stage_location+"/"+json_partition_dir[json_index], 
+                        auto_compress=False, overwrite=True, parallel=10)
+                    )
+        #put_result(file_element," => ",put_result[0].status)
+        json_index+=1  
     
     session.close()
     
